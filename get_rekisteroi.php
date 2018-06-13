3132e79cb5197b2f34ea0b7718f4a5e97bfa4ee6
Mika Vainio <koti.mika.vainio@gmail.com>
2018-06-13 10:43:14 +0300

    "toka"

---
+++ get_rekisteroi.php
@@ -0,0 +1,8 @@
+<?php
+
+//print_r($_GET);
+
+echo "Hei ".$_GET["sposti"];
+
+
+?>
--- reqister.php
+++ reqister.php
@@ -1,4 +1,14 @@
+<html>
+<form method="POST" action="post_rekisteroi.php">
+Sähköposti<input type="text" name="sposti"><br/>
+Salasana<input type="password" name="salasana"><br/>
+Salasana Uudestaan<input type="password" name="salasana_uudestaan"><br/>
+<input type="submit" value="Rekisteröidy">
+</form>
+</html>
+
 <?php
+/*
 $servername = "localhost";
 $username = "kirjautuminen";
 $password = "Q2werty";
@@ -11,4 +21,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+*/
 ?>
