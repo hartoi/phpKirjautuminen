6f9272711fd6dc42f79748a722014276961ef956
Mika Vainio <koti.mika.vainio@gmail.com>
2018-06-13 12:51:47 +0300

    Viides

--- reqister.php
+++ reqister.php
@@ -2,7 +2,7 @@
 $salasanaVirhe = "";
 $spostiVirhe = "";
 $spostiValue = "";
-$sqlVirheet = "";
+$sqlVirhe = "";
 
 
 if( $_SERVER["REQUEST_METHOD"] == "POST") {
@@ -24,28 +24,21 @@ if( $_SERVER["REQUEST_METHOD"] == "POST") {
         $servername = "localhost";
         $username = "kirjautuminen";
         $password = "Q2werty";
-
-
-        // Create connection
         $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } 
         echo "Connected successfully";
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('John', 'Doe', 'john@example.com')";
+        $sql = "INSERT INTO kirjautuminen.kayttajat (email,salasana) 
+        VALUES ('".$_POST["sposti"]."', '".$_POST["salasana"]."')";
         
         if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
+            echo "Tunnuksen luonti onnistui";
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        
-}
- 
+            $sqlVirhe = "Jotain meni vikaan.";
+        }     
+    }
 }
 ?>
 
